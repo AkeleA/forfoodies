f20c64a356f34f6b17a08ca4f0e097f3bf370c55
Akele <achorisaac@gmail.com>
2022-12-09 00:03:55 +0100

    40% done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 import { Link } from "react-router-dom";
 import imago from "../../Assets/images/loginPicture.svg";
 import Button from "../../Components/Button/Button";
 
 const Login = () => {
+  //defining the show password function
+  //first step useState
+  const [showPassword, setshowPassword] = useState(false);
+
+  /// the toggle password i.e show and hide
+  const togglePassword = (e) => {
+    //to prevent default action
+    e.preventDefault();
+
+    setshowPassword(!showPassword);
+  };
+  /// login data
+  const [logData, setlogData] = useState({ email: "", password: "" });
+
+  //handling form data
+  const handleChange = (e) => {
+    setlogData({
+      ...logData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //form control
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let logUser = JSON.parse(localStorage.getItem("user"));
+    console.log(logUser);
+    console.log(logData);
+    if (
+      logData?.email === logUser?.email &&
+      logData?.password === logUser?.password
+    ) {
+      setInterval(() => {
+        window.location = "/Dashboard";
+      }, 2000);
+    }
+  };
+
   return (
     <div classname="login">
       <div className="L-left">
@@ -12,22 +50,27 @@ const Login = () => {
       </div>
       <div className="log-right">
         <h2>Welcome Back!</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             className="mail-input"
             type="email"
             name="email"
             placeholder="Your Email address"
+            onChange={handleChange}
           />
           <div className="pass-div">
             <input
               className="sec-input"
+              type={showPassword ? "text" : "password"}
+              onChange={handleChange}
               placeholder="Your Password"
               name="password"
               id="password"
               required
             />
-            <span className="show-button">show</span>
+            <span className="show-button" onClick={togglePassword}>
+              show
+            </span>
           </div>{" "}
           <Button props={"LOGIN"} />
         </form>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Register.scss";
+import { Link } from "react-router-dom";
+import Button from "../../Components/Button/Button";
+import imago from "../../Assets/images/signupPicture.svg";
 
 const Register = () => {
-  return <div>Register</div>;
+  //toggle password
+  const [showPassword, setshowPassword] = useState(false);
+  const togglePassword = (e) => {
+    e.preventDefault();
+    setshowPassword(!showPassword);
+  };
+
+  ///Form controlling
+  const [formcheck, setformcheck] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  return (
+    <div className="register">
+      <div className="r-left">
+        <img src={imago} alt="" className="reg-img" />
+      </div>
+      <div className="r-right">
+        <h2>Welcome to Lilies!</h2>
+        <form>
+          <input
+            className="r-name"
+            type="text"
+            name="name"
+            placeholder="Your First Name"
+          />
+          <input
+            className="r-mail"
+            name="email"
+            type="email"
+            placeholder="Your Email address"
+          />{" "}
+          <div className="r-sec">
+            <input
+              className="r-password"
+              name="password"
+              placeholder="Your Password"
+              id="password"
+              required
+            />
+            <span className="show-button" onClick={togglePassword}>
+              show
+            </span>
+          </div>{" "}
+          <br />
+          {/**  <Button 
+          className={formvalid?"SignUp-active":"SignUp-submit"}
+          type='submit'
+          id='btn'
+          disabled={!formvalid}
+          props={"SIGN UP"} />**/}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
